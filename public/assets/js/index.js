08bbea9e4fe5823fe686da92e141f71e4f683ed2
Adriano Miranda <adrjmiranda@gmail.com>
2025-02-16 00:37:13 -0300

    feat: created input password visibility toggle functionality

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("console.log(\"It's working!\");\n\n\n//# sourceURL=webpack://register-page-design/./src/index.ts?");
+eval("/**\n * Changes Password Visibility\n */\nvar passwordInput = document.querySelector('#password');\nvar passwordConfirmationInput = document.querySelector('#password-confirmation');\nvar passwordVisibilityButton = document.querySelector('.password-visibility');\nvar passwordConfirmationVisibilityButton = document.querySelector('.password-confirmation-visibility');\nvar HIDDEN_CLASS = 'hidden';\nvar changeVisibility = function (_a) {\n    var _b;\n    var input = _a.input, button = _a.button;\n    var inputType = (_b = input.getAttribute('type')) !== null && _b !== void 0 ? _b : 'password';\n    var newType = inputType === 'text' ? 'password' : 'text';\n    input.setAttribute('type', newType);\n    button.querySelectorAll('i').forEach(function (icon) {\n        return icon.classList.toggle(HIDDEN_CLASS);\n    });\n};\nif (passwordInput &&\n    passwordVisibilityButton &&\n    passwordConfirmationInput &&\n    passwordConfirmationVisibilityButton) {\n    var inputFieldList = [\n        {\n            input: passwordInput,\n            button: passwordVisibilityButton,\n        },\n        {\n            input: passwordConfirmationInput,\n            button: passwordConfirmationVisibilityButton,\n        },\n    ];\n    inputFieldList.forEach(function (inputField) {\n        inputField.button.addEventListener('click', function () {\n            return changeVisibility(inputField);\n        });\n    });\n}\n\n\n//# sourceURL=webpack://register-page-design/./src/index.ts?");
 
 /***/ })
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,56 @@
-console.log("It's working!");
+/**
+ * Changes Password Visibility
+ */
+const passwordInput = document.querySelector('#password') as HTMLInputElement;
+const passwordConfirmationInput = document.querySelector(
+	'#password-confirmation'
+) as HTMLInputElement;
+
+const passwordVisibilityButton = document.querySelector(
+	'.password-visibility'
+) as HTMLButtonElement;
+const passwordConfirmationVisibilityButton = document.querySelector(
+	'.password-confirmation-visibility'
+) as HTMLButtonElement;
+
+interface InputField {
+	input: HTMLInputElement;
+	button: HTMLButtonElement;
+}
+
+const HIDDEN_CLASS: string = 'hidden';
+
+const changeVisibility = ({ input, button }: InputField): void => {
+	const inputType: string = input.getAttribute('type') ?? 'password';
+	const newType: string = inputType === 'text' ? 'password' : 'text';
+
+	input.setAttribute('type', newType);
+
+	(button.querySelectorAll('i') as NodeListOf<HTMLElement>).forEach((icon) =>
+		icon.classList.toggle(HIDDEN_CLASS)
+	);
+};
+
+if (
+	passwordInput &&
+	passwordVisibilityButton &&
+	passwordConfirmationInput &&
+	passwordConfirmationVisibilityButton
+) {
+	const inputFieldList: Array<InputField> = [
+		{
+			input: passwordInput,
+			button: passwordVisibilityButton,
+		},
+		{
+			input: passwordConfirmationInput,
+			button: passwordConfirmationVisibilityButton,
+		},
+	];
+
+	inputFieldList.forEach((inputField: InputField) => {
+		inputField.button.addEventListener('click', () =>
+			changeVisibility(inputField)
+		);
+	});
+}
